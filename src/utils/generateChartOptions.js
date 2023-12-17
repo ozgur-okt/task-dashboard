0e6293b0627694f1e793e05583566a5a8c48f2ef
Özgür Oktay <oktayozgur94@gmail.com>
2023-12-17 22:51:31 +0300

    y range changed for temperature

--- src/utils/generateChartOptions.js
+++ src/utils/generateChartOptions.js
@@ -2,7 +2,7 @@ export const generateChartOptions = (name, yAxis, chart, stroke, grid, xAxis, ma
   const color = '#46a2e0' //blue
   const title = name.toUpperCase()
   const formatter = name === 'altitude' ? (value) => { return value ? value.toFixed(0) + ' m' : '' } : (value) => { return value + (name === 'temperature' ? ' °C' : ' m/s') }
-  const minMax = name === 'altitude' ? { min: -40000, max: 40000 } : name === 'temperature' ? { min: -90, max: 90 } : { min: -150, max: 150 }
+  const minMax = name === 'altitude' ? { min: -40000, max: 40000 } : name === 'temperature' ? { min: -60, max: 60 } : { min: -150, max: 150 }
 
   return {
     options: {
