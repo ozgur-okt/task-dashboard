9a95d9fab495c5a51381023a91cc2d5b9f600980
Özgür Oktay <oktayozgur94@gmail.com>
2023-12-17 19:36:40 +0300

    action required component

---
+++ src/components/modals/ModalActionRequired.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { modalStyles } from '../../utils/modals/modalStyles'
+import { actOnSpectrum } from '../../redux/actions/spectrumActions'
+import Modal from 'react-modal'
+import '../../styles/components/modals/modal-action-required.scss'
+
+const ModalActionRequired = () => {
+  const dispacth = useDispatch()
+  const isActionRequired = useSelector(state => state.spectrum.isActionRequired)
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const takeAction = () => {
+    setModalOpen(false)
+    dispacth(actOnSpectrum())
+  }
+
+  useEffect(() => {
+    if (isActionRequired) {
+      setModalOpen(true)
+    }
+  }, [isActionRequired])
+
+  Modal.setAppElement('body')
+
+  return (
+    <Modal isOpen={modalOpen} style={modalStyles}>
+      <h1 className='modal-title'>CRITICAL ACTION REQUIRED</h1>
+      <button className='modal-button' onClick={() => takeAction()}>TAKE ACTION</button>
+    </Modal>
+  )
+}
+
+export default ModalActionRequired
---
+++ src/utils/modals/modalStyles.js
@@ -0,0 +1,16 @@
+export const modalStyles = {
+  overlay: {
+    backgroundColor: 'rgba(255, 0, 0, 0.433)',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    width: '40%',
+    height: '40%',
+    margin: 'auto',
+    border: 'none',
+    backgroundColor: 'gray',
+  }
+}
